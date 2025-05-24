1063c6f437bb4f03abb82e687dbc182d063e63c9
Mouad <elba0072@algonquinlive.com>
2025-05-24 13:48:02 -0400

    Lab_3 update

--- shopping_cart.py
+++ shopping_cart.py
@@ -6,12 +6,14 @@
 
 print("Welcome to the Shopping Cart Program!")
 user_input = ""
-while user_input != "n":
+# While loop to restart the coded if the user want to
+while user_input != "no":
     try:
+        # Asking the user for the number of items
         numOfItems = int(input("Please enter the Number of items: "))
         count = 0
         total_cost = 0
-
+        # While loop to calculate the total cost
         while count < numOfItems :
             try:
                 name =input(f"What is the name of item number {count+1}: ")
@@ -19,21 +21,23 @@ while user_input != "n":
                 quantity = float(input("How many did you get: "))
                 total = price * quantity
                 total_cost += total
+            # This except block is to catch any invalid input for price and quantity    
             except:
                 print("Could not calculate the total")
                 count = numOfItems
             count+=1
-            
+        # Displaying the total cost
         print(f"The total cost of your Purchase is ${total_cost:.2f}")
-
+        # Adding a discount if the total coast is greater than 100$
         if total_cost > 100:
             discount = total_cost * 0.1
             total_cost -= discount
             print(f"\nYou saved ${discount:.2f} with a 10% discount!")
             print(f"Discounted Total: ${total_cost:.2f}")
-            
+    # This except block is to catch any invalid Number of items      
     except:
-        print("Invalid number f items input")
-
+        print("Invalid number of items")
+    # checking if the user want to restart the code
     user_input = input("\nWould you like to shop again? (yes/no): ").lower()
+
 print("Thank you for shopping with us!")
